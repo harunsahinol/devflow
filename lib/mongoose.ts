98dc439f9986136bbe5e7c40a844e106c7401237
Harun Sahinol <mehmethsh@gmail.com>
2024-11-29 18:02:57 +0300

    Setup MongoDB and Mongoose

---
+++ lib/mongoose.ts
@@ -0,0 +1,54 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
+}
+
+interface MongooseCache {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+declare global {
+  // eslint-disable-next-line no-var
+  var mongoose: MongooseCache;
+}
+
+let cached = global.mongoose as MongooseCache;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+const dbConnect = async (): Promise<Mongoose> => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      dbName: "devflow",
+    };
+
+    cached.promise = mongoose
+      .connect(MONGODB_URI, opts)
+      .then((result) => {
+        console.log("Connected to MongoDB");
+        return result;
+      })
+      .catch((error) => {
+        console.error("Error connecting to MongoDB:", error);
+        throw error;
+      });
+  }
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+};
+
+export default dbConnect;
