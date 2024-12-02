c58103791ff1183d8a306b2569c0bbe2ee64aa2c
Harun Sahinol <mehmethsh@gmail.com>
2024-12-02 22:48:59 +0300

    Answer, Interaction, Tag, Vote Models

--- database/template.model.ts
+++ database/template.model.ts
@@ -2,8 +2,8 @@ import { model, models, Schema } from "mongoose";
 
 export interface IModel {}
 
-const QuestionSchema = new Schema({}, { timestamps: true });
+const ModelSchema = new Schema({}, { timestamps: true });
 
-const Model = models?.Model || model<IModel>("Question", QuestionSchema);
+const Model = models?.Model || model<IModel>("Model", ModelSchema);
 
 export default Model;
