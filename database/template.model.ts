c600acbc558eb9f69569c6eb8f8cc064d76ec16a
Harun Sahinol <mehmethsh@gmail.com>
2024-12-03 11:06:35 +0300

    Implement Error Handling

--- database/template.model.ts
+++
@@ -1,9 +0,0 @@
-import { model, models, Schema } from "mongoose";
-
-export interface IModel {}
-
-const ModelSchema = new Schema({}, { timestamps: true });
-
-const Model = models?.Model || model<IModel>("Model", ModelSchema);
-
-export default Model;
